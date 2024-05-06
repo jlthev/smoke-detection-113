dfa3adb890d7e02469d57c0a554ed415ea0b0b89
Joseph <72987196+jlthev@users.noreply.github.com>
2024-05-06 09:41:39 -0400

    Create tempature.py

---
+++ tempature.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# Script prints an RPi's temperature to the command line
+# tintc, tintf = temperature (in deg C), temperature (in deg F)
+# Written by Joseph Loeffler
+# Last update - 5/6/24
+
+from subprocess import call, check_output
+from time import sleep
+
+temp_range = []
+sleep_duration = 5
+n = 0
+while True:
+    tact = check_output(['cat','/sys/class/thermal/thermal_zone0/temp'])
+    tintc = int(tact) / 1000
+    tintf = tintc * (9/5) + 32
+    temp_range.append(tintc)
+    print(f'RPi CPU Temp: {tintc:.2f} deg C = {tintf:.2f} deg F | Range:{min(temp_range):.2f}-{max(temp_range):.2f} over {n*sleep_duration} sec')
+    n+=1
+    sleep(sleep_duration)
