db6c24c2fb8bc92e38b4d9239716940092a851b9
NikoKrontiris <167633183+NikoKrontiris@users.noreply.github.com>
2024-05-14 08:59:38 -0400

    Niko: normalize function updated

--- arduinosmoke.ino
+++ arduinosmoke.ino
@@ -9,6 +9,10 @@ int left_data = 0;
 int mid_data = 0;
 int right_data = 0;
 
+float left_threshold = 0;
+float mid_threshold = 0;
+float right_threshold = 0;
+
 //#include <string>
 
 //state0 = 0 //nothing
@@ -23,14 +27,57 @@ int right_data = 0;
 void setup() {
   Serial.begin(9600);           //  setup serial
 }
+
 void normalize(){
   // Function to normalize data during the first t seconds of operation
   // Return values to check against in the later if statements
   // Pseudo code
+
+  //New functionality: Wait five-to-ten seconds upon startup.
+  //Take average of readings across each sensor for five seconds.
+  //Each average represents the baseline gas readings of a normal room.
+  //Those averages become the thresholds for each sensor, respectively.
+
+  delay(500);
+
+  float sum_left50 = 0;
+  for (int i = 0; i < 50; i++){ // take fifty readings
+      analogRead(leftPin);
+      sum_left50 += leftPin;
+  }
+  float average_left = sum_left50 / 50;
+
+  float sum_right50 = 0;
+  for (int i = 0; i < 50; i++){ // take fifty readings
+      analogRead(rightPin);
+      sum_right50 += rightPin;
+  }
+  float average_right = sum_right50 / 50;
+
+
+  float sum_mid50 = 0;
+  for (int i = 0; i < 50; i++){ // take fifty readings
+      analogRead(midPin);
+      sum_mid50 += midPin;
+  }
+  float average_mid = sum_mid50 / 50;
+
+
+  left_threshold = average_left;
+  right_threshold = average_right;
+  mid_threshold = average_mid;
+
+  //return (left_threshold,);
+  //return (right_threshold);
+  //return (mid_threshold);
+
 }
 
 void loop() {
   String current_state = "";
+  
+  normalize(); // calls normalize function, which takes 5-10 seconds depending on the setting
+  
   left_data = analogRead(leftPin);  // read the input pin
   Serial.print(left_data);          // debug value
 
@@ -49,13 +96,13 @@ void loop() {
   //threshhold for left: 300 MODIFIED TO 250 
   //threshold for right: 250
 
-  if(left_data >= 250){
+  if(left_data >= left_threshold){
     current_state += "1";
   }
-  if(mid_data >= 250){
+  if(mid_data >= mid_threshold){
     current_state += "2";
   }
-  if(right_data >= 250){
+  if(right_data >= right_threshold){
     current_state += "3";
   }
   if(current_state == ""){
